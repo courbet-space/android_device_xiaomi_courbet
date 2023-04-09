2b83659ed5a345056a0c529591356cee9810dc94
youngherna <hernan.mauricio16@gmail.com>
2023-04-09 15:27:06 +0000

    courbet: DerpFest support

--- derp_courbet.mk
+++ derp_courbet.mk
@@ -12,12 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_courbet
+PRODUCT_NAME := derp_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Mi 11 Lite 4G
+PRODUCT_MODEL := Mi 11 Lite
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
